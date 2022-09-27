470138a6691ebd614d3190b2bc89d2fc62da21e7
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2022-09-27 21:17:27 +0300

    test b

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,12 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox"
-import { galleryItems } from './gallery-items';
+import {
+  galleryItems
+} from './gallery-items';
 // Change code below this line
-console.log(SimpleLightbox);
-console.log(galleryItems);
+// console.log(SimpleLightbox);
+// console.log(galleryItems);
+const a = 15
+b = 10;
+console.log(a);
+console.log(b);
